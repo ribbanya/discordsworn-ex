771d51cb6e72dbff07b9ef8fa3ccce731e18e806
Robin Avery <ribbanya@gmail.com>
2020-04-10 12:34:06 -0400

    Move token to system environment variable

--- config/config.exs
+++ config/config.exs
@@ -2,8 +2,12 @@ import Config
 
 config :ex_dice_roller,
   cache_table: ExDiceRoller.Cache
+
 config :nostrum,
-  token: "NDY5NTE0NTEwODUyNDIzNjgx.XoTJpw.Snf3K0KfbEk-eElVqP1wLvjz_Fc", # The token of your bot as a string
-  num_shards: :auto # The number of shards you want to run your bot under, or :auto.
+  # The token of your bot as a string
+  token: System.get_env("DISCORDSWORN_TOKEN"),
+  # The number of shards you want to run your bot under, or :auto.
+  num_shards: :auto
+
 config :logger,
   level: :debug
